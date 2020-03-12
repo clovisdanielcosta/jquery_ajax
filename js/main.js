6c5e2e68a8e4e819ea9b66a9cdd8734f478864c1
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2020-03-12 03:53:02 -0300

    App Consulta CEP final

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function consultaCep() {
-    
+    $(".barra-progresso").show();
     var cep = document.getElementById("cep").value;
     var url = "https://viacep.com.br/ws/" + cep + "/json/"; 
     
@@ -8,13 +8,23 @@ function consultaCep() {
         type: "GET",
         success: function name(response) {
             console.log(response);
+            $("#titulo_cep").html("CEP: " + response.cep);
             $("#logradouro").html(response.logradouro);
-            //document.getElementById("logradouro").innerHTML = response.logradouro;
-            document.getElementById("localidade").innerHTML = response.localidade;
-            document.getElementById("complemento").innerHTML = response.complemento;
-            document.getElementById("bairro").innerHTML = response.bairro;
-            document.getElementById("uf").innerHTML = response.uf;
-            document.getElementById("cep2").innerHTML = response.cep;
+            $("#localidade").html(response.localidade);
+            $("#complemento").html(response.complemento);
+            $("#bairro").html(response.bairro);
+            $("#uf").html(response.uf);
+            $(".cep").show();
+            $(".barra-progresso").hide();
+
+            /*  Outra forma sem JQuery
+                document.getElementById("logradouro").innerHTML = response.logradouro;
+            */
         }
     })
 }
+
+$(function(){
+    $(".cep").hide();
+    $(".barra-progresso").hide();
+});
